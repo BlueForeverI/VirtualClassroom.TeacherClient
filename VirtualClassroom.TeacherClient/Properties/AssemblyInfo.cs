7e6b20461741c450f8ef52cd658ca854d5c802ec
George <bluef0rever@abv.bg>
2013-03-07 16:56:40 +0200

    Edited some assembly metadata.

--- VirtualClassroom.TeacherClient/Properties/AssemblyInfo.cs
+++ VirtualClassroom.TeacherClient/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtualClassroom.TeacherClient")]
+[assembly: AssemblyTitle("Виртуална класна стая - Учител")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtualClassroom.TeacherClient")]
+[assembly: AssemblyCompany("PMG Lovech Soft")]
+[assembly: AssemblyProduct("Учител")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
